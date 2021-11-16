8a8b1f4aba03c2c2c4422ed5f02c65d0acd5ba6d
Weverton <weverton@contatogoldenskillcom.onmicrosoft.com>
2021-11-16 18:14:10 -0300

    Add FichaConsultaImpressão

---
+++ Pages/Cliente/FichaConsultaImpressao.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OpenHealthWeb.Pages.Profissional
+{
+    public class FichaConsultaImpressaoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

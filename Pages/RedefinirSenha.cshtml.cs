71d8a857932387ac6cfeed34fec57b63dee5e736
Tiago Freitas <tiagofreitastjf@outlook.com.br>
2021-11-19 21:29:35 -0300

    Correções

--- Pages/RedefinirSenha.cshtml.cs
+++ Pages/RedefinirSenha.cshtml.cs
@@ -6,28 +6,8 @@ namespace OpenHealthWeb.Pages
 {
     public class RedefinirSenhaModel : PageModel
     {
-        [BindProperty]
-        public Redefinir Redefinir { get; set; }
-        public string[] Tipos = new[] { "Paciente", "Profissional" };
-
         public void OnGet()
         {
         }
-
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (Redefinir.Senha != null && Redefinir.Senha != null)
-            {
-
-            }
-            return null;
-        }
-    }
-
-    public class Redefinir
-    {
-        public string SenhaConfirmar { get; set; }
-        public string Senha { get; set; }
-        public string TipoLogin { get; set; } = "Paciente";
     }
 }

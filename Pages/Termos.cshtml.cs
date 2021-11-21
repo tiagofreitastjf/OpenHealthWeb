6b6d57afa781cdd952f0d1378daf59f486ad7366
Tiago Freitas <tiagofreitastjf@outlook.com.br>
2021-11-21 20:57:24 -0300

    Termos de uso

---
+++ Pages/Termos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OpenHealthWeb.Pages
+{
+    public class TemosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
